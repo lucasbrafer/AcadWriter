df122b2cbbadf3a899b2df6241861af327a1fafc
Lucas Braga Ferreira <lucasbrafer@gmail.com>
2021-10-21 22:33:55 -0300

    javascript architeture

---
+++ Frontend/acad-writer/src/App.jsx
@@ -0,0 +1,12 @@
+import Editor from './components/Editor/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world! </h1>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
