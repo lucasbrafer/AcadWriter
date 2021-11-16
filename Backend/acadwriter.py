94e8614eb6c7daf0c607eae9041c706330b90e62
Lucas Braga Ferreira <lucasbrafer@gmail.com>
2021-11-15 21:22:29 -0300

    resolve cors json source and request get

--- Backend/acadwriter.py
+++ Backend/acadwriter.py
@@ -1,21 +1,27 @@
 from flask import Flask, request, jsonify
 from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel, BertTokenizer, BertModel, BertForMaskedLM
 from transformers import pipeline
+from flask_cors import CORS, cross_origin
 import re
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/", methods = ['POST'])
-def hello_world():
-    text = request.form['text']
+
+@app.route("/sugestion", methods=['POST'])
+@cross_origin(origin='localhost')
+def sugestions():
+    text = request.get_json()['text']
     text = text.strip()
     text = basicPreprocess(text)
 
     results = fill_mask(text + " [MASK]")
 
-    words = [fill_mask.tokenizer.convert_ids_to_tokens(a["token"]) for a in list(results)]
+    words = [fill_mask.tokenizer.convert_ids_to_tokens(
+        a["token"]) for a in list(results)]
 
     return jsonify(words)
+
 
 def basicPreprocess(text):
     processed_text = text.lower()
@@ -24,4 +30,6 @@ def basicPreprocess(text):
     processed_text = re.sub(r" +", " ", processed_text)
     return processed_text
 
-fill_mask = pipeline("fill-mask", model="allenai/scibert_scivocab_uncased", tokenizer="allenai/scibert_scivocab_uncased")
+
+fill_mask = pipeline("fill-mask", model="allenai/scibert_scivocab_uncased",
+                     tokenizer="allenai/scibert_scivocab_uncased")
